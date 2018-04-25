88a12d95e5c6336733d821954ec508c55cd42201
lettda <lettda@vcu.edu>
2018-04-25 16:08:46 -0400

    N/A

--- Program.cs
+++ Program.cs
@@ -6,13 +6,12 @@ namespace _350_final_project
 {
     class Program
     {
-
         public static void Main()
         {
             string playerName;
             string playerClass;
 
-            Story newStory = new Story(); //instantiate new class to access class member methods
+            Story newStory = new Story(); //instantiate new class to access class member methods (main method being static)
 
             do
             {
@@ -26,12 +25,10 @@ namespace _350_final_project
             WriteLine("Choose your class by entering its name: \n You may choose the noble Warrior, the dastardly Theif, or the honorable Gunner");
             playerClass = ReadLine(); //Have player input their class and save for further use
 
-            newStory.ChooseClass(playerClass); 
+            newStory.ChooseClass(playerClass); //Belongs to the story class, must have an instance instatiated due to main method being static
 
 
-            newStory.Beginning(playerName, playerClass);
+            newStory.Beginning(playerName, playerClass); //Belongs to the story class
         }
-
-
     }
 }
